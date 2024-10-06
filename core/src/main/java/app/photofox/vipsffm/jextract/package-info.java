10c950737b790a4631efab3486b5c73a5f91cf04
carrot <149851+lopcode@users.noreply.github.com>
2024-10-06 23:08:18 +0100

    Fix docs for 1.1 release (#105)

---
+++ core/src/main/java/app/photofox/vipsffm/jextract/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <p>Automatically generated bindings from the jextract tool</p>
+ *
+ * <p><b>API compatibility is not guaranteed across minor versions for classes in this package - things may change!</b></p>
+ */
+package app.photofox.vipsffm.jextract;
