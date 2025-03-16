28abe18b930048d0dfec4c7559b08d3b13f18357
sage <149851+lopcode@users.noreply.github.com>
2025-03-16 16:45:59 +0000

    Add Ubuntu 24.04 to Docker tests (#142)
    * Add Ubuntu 24.04 to Docker tests
    
    * Don't obscure underlying cause of invalid library path exception
    
    * Bump to 1.5.2

--- run_docker_tests.sh
+++ run_docker_tests.sh
@@ -7,9 +7,13 @@ echo "building samples..."
 echo "running docker tests..."
 WORKSPACE_DIR="$PWD"
 
-pushd docker_tests/debian-12
-cp -r "$WORKSPACE_DIR"/sample .
-cp "$WORKSPACE_DIR"/run_samples.sh .
-docker build -t vips-ffm-debian-test .
-docker run vips-ffm-debian-test
-popd
+docker_tests=("debian-12" "ubuntu-2404")
+for docker_test in "${docker_tests[@]}"; do
+  echo "testing \"$docker_test\""
+  pushd "docker_tests/$docker_test"
+  cp -r "$WORKSPACE_DIR"/sample .
+  cp "$WORKSPACE_DIR"/run_samples.sh .
+  docker build -t "vips-ffm-$docker_test-test" .
+  docker run "vips-ffm-$docker_test-test"
+  popd
+done
